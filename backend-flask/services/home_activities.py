13db265ffe6bc4323c93c80a317aa2af632e68bc
Marciemaps <marciam987@gmail.com>
2023-03-22 09:24:34 +0000

    Cloudwatch Logs

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,14 +1,16 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+import logging
 
 tracer= trace.get_tracer("home.activities")
 
 class HomeActivities:
- def run():
+ def run(logger):
+   logger.info("HomeActivities")
    with tracer.start_as_current_span("home-activities-mock-data"):
     span = trace.get_current_span()
     now = datetime.now(timezone.utc).astimezone()
-    span.set_attribute("app.now", now.isoformant())
+    span.set_attribute("app.now",now.isoformat())
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
