918bb6261f8321237aa531e6324b8c64c330e568
Marciemaps <marciam987@gmail.com>
2023-04-10 07:55:07 +0000

    HomeFeedPage.js

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -40,33 +40,31 @@ export default function HomeFeedPage() {
     }
   };
 
-  const checkAuth = async () => {
-    Auth.currentAuthenticatedUser({
-      // Optional, By default is false. 
-      // If set to true, this call will send a 
-      // request to Cognito to get the latest user data
-      bypassCache: false 
-    })
-    .then((user) => {
-      console.log('user',user);
-      return Auth.currentAuthenticatedUser()
-    }).then((cognito_user) => {
-        setUser({
-          display_name: cognito_user.attributes.name,
-          handle: cognito_user.attributes.preferred_username
-        })
-    })
-    .catch((err) => console.log(err));
-  };
+// check if we are authenticated
+const checkAuth = async () => {
+ Auth.currentAuthenticatedUser({
+   // Optional, By default is false. 
+   // If set to true, this call will send a 
+   // request to Cognito to get the latest user data
+   bypassCache: false 
+ })
+ .then((user) => {
+    console.log('user',user);
+    return Auth.currentAuthenticatedUser()
+ }).then((cognito_user) => {
+     setUser({
+       display_name: cognito_user.attributes.name,
+       handle: cognito_user.attributes.preferred_username
+     })
+ })
+  .catch((err) => console.log(err));
+ };
   
-  React.useEffect(()=>{
-    //prevents double call
-    if (dataFetchedRef.current) return;
-    dataFetchedRef.current = true;
-
-    loadData();
-    checkAuth();
-  }, [])
+// check when the page loads if we are authenicated
+React.useEffect(()=>{
+  loadData();
+  checkAuth();
+}, [])
 
   return (
     <article>
